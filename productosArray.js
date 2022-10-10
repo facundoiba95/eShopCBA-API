fca3dcc2f82f9d30a169fd03551b0c0dca1dc12a
facundoiba95 <facundoiba95@gmail.com>
2022-10-10 16:48:24 -0300

    inicio de proyecto

---
+++ productosArray.js
@@ -0,0 +1,201 @@
+let productos = [
+    [
+        {"smartphones:": [ 
+
+            {
+                "iphone": [
+
+                {
+                    "id": 1,
+                    "name": "iPhone 12 Pro Max",
+                    "price": 410000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/Apple-iPhone-12-Pro-Max.png?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 2,
+                    "name": "iPhone 12",
+                    "price": 290000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone12.png?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 3,
+                    "name": "iPhone 13",
+                    "price": 300000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone13.png?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 4,
+                    "name": "iPhone 13 Pro Max",
+                    "price": 380000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone_13_pro_verde_03_ad_l.jpg?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 5,
+                    "name": "iPhone 13 Pro Max",
+                    "price": 380000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone_13_pro_verde_03_ad_l.jpg?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 6,
+                    "name": "iPhone 13 Pro Max",
+                    "price": 380000,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone_13_pro_verde_03_ad_l.jpg?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+                {
+                    "id": 7,
+                    "name": "iPhone 11",
+                    "price": 188500,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/iphone/iphone_13_pro_verde_03_ad_l.jpg?raw=true",
+                    "free-shipping":true,
+                    "tradename":"iPhone"
+                },
+
+            ]
+            },
+            {
+                "samsung": [
+                {
+                    "id": 8,
+                    "name": "Samsung SM-A505",
+                    "price": 54600,
+                    "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/celulares/samsung/SM-A505_001_Front.png?raw=true",
+                    "free-shipping":true,
+                    "tradename": "Samsgung"
+                },
+            ]
+        
+            }
+            
+        
+        ]
+    }
+],
+[
+    {"tablets": [
+
+        {"iPad": [
+            {
+                "id": 9,
+                "name": "iPad Air 4",
+                "price": 109000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/tablets/iphone/Apple_iPad_Air_4.png?raw=true",
+                "free-shipping":true,
+                "tradename":"iPhone"
+            },
+            {
+                "id": 10,
+                "name": "iPad Pro",
+                "price": 204000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/tablets/iphone/Ipad-pro.png?raw=true",
+                "free-shipping":true,
+                "tradename":"iPhone"
+            },
+
+        ]},
+        {"samsung": [
+            {
+                "id": 11,
+                "name": "Samsung Tab S6 Lite",
+                "price": 75000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/tablets/samsung/samsung-tabS6lite.png?raw=true",
+                "free-shipping":true,
+                "tradename":"Samsung"
+            },
+
+        ]}
+    ]}
+],
+[
+    {"headsets": [
+        {"AKG": [
+            {
+                "id": 12,
+                "name": "AKG k420 Studio",
+                "price": 40000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/headsets/AKG_k240_studio.png?raw=true",
+                "free-shipping":true,
+                "tradename":"AKG"
+            },    
+        ]},
+        {"corsair": [
+            {
+                "id": 13,
+                "name": "Corsair Virtuoso Special Edition",
+                "price": 45000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/headsets/corsair%20virtuoso%20special%20edition.png?raw=true",
+                "free-shipping":true,
+                "tradename":"Corsair"
+            },   
+        ]},
+    
+    ]}
+],
+[
+    {"notebooks": [
+        {"asus": [
+            {
+                "id": 14,
+                "name": "ASUS x509MA Intel n4020",
+                "price": 131950,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/netbook/asus%20x509MA%20intel%20n4020.png?raw=true",
+                "free-shipping":true,
+                "tradename":"ASUS"
+            },     
+        ]},
+        {"lenovo": [
+            {
+                "id": 15,
+                "name": "Lenovo IP Gaming 3",
+                "price": 131950,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/netbook/lenovo%20ip%20gaming%203.png?raw=true",
+                "free-shipping":true,
+                "tradename":"Lenovo"
+            },   
+            {
+                "id": 16,
+                "name": "Lenovo Thinkbook 15",
+                "price": 147000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/netbook/lenovo%20thinkbook%2015.png?raw=true",
+                "free-shipping":true,
+                "tradename":"Lenovo"
+            },  
+        ]},
+        {"razer": [
+            {
+                "id": 17,
+                "name": "Razer Blade 15",
+                "price": 598000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/netbook/Razer-Blade-15.jpg?raw=true",
+                "free-shipping":true,
+                "tradename":"Razer"
+            },  
+        ]},
+        {"dell": [
+            {
+                "id": 18,
+                "name": "Dell Inspiron n5559",
+                "price": 200000,
+                "image":"https://github.com/facundoiba95/eShopCBA-API/blob/master/netbook/dell%20inspiron%20n5559.png?raw=true",
+                "free-shipping":true,
+                "tradename":"Dell"
+            },    
+        ]}
+    ]}
+]
+        
+]
+
+
+module.exports = productos;
